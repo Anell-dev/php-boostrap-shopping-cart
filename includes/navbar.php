b495af35c166eade722db584d51372018384694b
Edwin Anel González <aneh6272@gmail.com>
2024-09-29 17:58:34 -0500

    adding logic for cart, also more styles

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,18 @@
-<nav class="container__principal-nav">
-    <div class="container__logotipe">
-        Logotipo
+<div class="conatiner-up-to-navbar">
+    <div>
+        <p>PANAMA</p>
+        <i class="bi bi-chevron-down"></i>
     </div>
+    <div>
+        <p>SING IN</p>
+        <p>WHISTLIST</p>
+    </div>
+</div>
+
+<nav class="container__principal-nav">
+    <p class="container__logotipe">
+        NEXLINE
+    </p>
 
     <ul class="container__nav-list">
         <li class="container__nav-items">
@@ -23,32 +34,34 @@
         </li>
     </ul>
 
-    <div class="search-container">
-        <input type="text" class="search-input" placeholder="Search...">
-        <i class="bi bi-search search-icon"></i> <!-- Icono de búsqueda -->
-    </div>
+    <div class="container-barSearch-and-cart">
+        <div class="search-container">
+            <input type="text" class="search-input" placeholder="Search...">
+            <i class="bi bi-search search-icon"></i> <!-- Icono de búsqueda -->
+        </div>
 
-    <!--  carrito con contador -->
-    <div class="cart-container">
-        <button class="cart-button">
-            <i class="bi bi-cart"></i>
-            <!-- Aqui puedes declarar la variable y cuenta la cantidad de productos en el carrito y los agregas aqui------[0] -->
-            <span class="cart-count">0</span> <!-- Contador -->
-        </button>
+        <!--  carrito con contador -->
+        <div class="cart-container">
+            <button class="cart-button">
+                <i class="bi bi-cart"></i>
+                <!-- Aqui puedes declarar la variable y cuenta la cantidad de productos en el carrito y los agregas aqui------[0] -->
+                <span class="cart-count">0</span> <!-- Contador -->
+            </button>
+        </div>
     </div>
 </nav>
 
 <div class="container__principal-section">
     <div id="home" class="container__section-items">
-        <h3>HOME</h3>
+        <h6>HOME</h6>
     </div>
     <div id="add-product" class="container__section-items">
-        <h3>ADD PRODUCT</h3>
+        <h6>ADD PRODUCT</h6>
     </div>
     <div id="carrusel-product" class="container__section-items">
-        <h3>CARRUCEL PRODUCT</h3>
+        <h6>CARRUCEL PRODUCT</h6>
     </div>
     <div id="store-card" class="container__section-items">
-        <h3>STORE CART</h3>
+        <h6>STORE CART</h6>
     </div>
 </div>
--- views/storecar.php
+++ views/storecar.php
@@ -2,21 +2,60 @@
 <html lang="en">
 
 <head>
-    <title>Edwin Gonzales -Page</title>
+    <title>Cart</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../assets/css/navbar.css">
+
     <link rel="stylesheet" href="../assets/css/carrito.css">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
 </head>
 
 <body>
     <?php require("../includes/navbar.php"); ?>
 
-    <h1>Carrito de compras</h1>
+    <div class="container-shopping-cart">
+        <h4>Carrito de compras</h4>
 
+        <hr>
+        <div class="container-header-shopping-cart">
+            <p>PRODUCT DETAILS</p>
+            <p>QUANTITY</p>
+            <p>PRICE</p>
+            <p>TOTAL</p>
+        </div>
+        <hr>
+
+        <div class="container-product-details">
+            <div class="container-img-and-details">
+                <img src="../assets/img/laptops-2048px-8826.webp" alt="" srcset="">
+                <div>
+                    <p>Laptop Lenovo</p>
+                    <p></p>
+                    <p></p>
+                    <p>Product Code: efefefefef</p>
+                </div>
+            </div>
+            <div class="container-quantity">
+                <div class="container-add-amount-remove">
+                    <p>-</p>
+                    <p>1</p>
+                    <p>+</p>
+                </div>
+                <p>REMOVE ALL</p>
+            </div>
+            <div>
+                <p>$35.00</p>
+            </div>
+            <div>
+                <p>$35.00</p>
+            </div>
+        </div>
+
+    </div>
 
 
 
